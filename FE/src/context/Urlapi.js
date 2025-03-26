4f191337496f57f0c238c36a602a17665b7f9dbc
romirahmann <romirahman03romi@gmail.com>
2025-03-26 10:31:59 +0700

    NEW: UPDATE

--- FE/src/context/Urlapi.js
+++ FE/src/context/Urlapi.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-export const ApiUrl = createContext("http://192.168.9.208:3002/api");
-// export const ApiUrl = createContext("http://192.168.9.192:3002/api");
+// export const ApiUrl = createContext("http://192.168.9.208:3002/api");
+export const ApiUrl = createContext("http://192.168.9.192:3002/api");
 // export const ApiUrl = createContext("http://localhost:3002/api");
